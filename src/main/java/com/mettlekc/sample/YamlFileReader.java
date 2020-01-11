53fe2fd0f7957458705685a6ab372f7ae1da9a63
mettlekc <mettlekc@gmail.com>
2020-01-11 23:52:21 +0900

    change group files format

--- src/main/java/com/mettlekc/sample/YamlFileReader.java
+++ src/main/java/com/mettlekc/sample/YamlFileReader.java
@@ -8,12 +8,17 @@ import java.util.Map;
 public class YamlFileReader {
 
     public final static void main(String[] args) {
+        Map<String, Object> load = load("aerospike-profile.yaml");
+        System.out.println(load);
+        System.out.println(load("mysql-profile.yaml"));
+    }
+
+    private static Map<String, Object> load(final String fileName) {
         Yaml yaml = new Yaml();
         InputStream inputStream = YamlFileReader.class
                 .getClassLoader()
-                .getResourceAsStream("aerospike-profile.yaml");
-        Map<String, Object> obj = yaml.load(inputStream);
-        System.out.println(obj);
+                .getResourceAsStream(fileName);
+        return yaml.load(inputStream);
     }
 
 }
